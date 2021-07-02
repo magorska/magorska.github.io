7af7ab25b8c40612246286056eac163cf030f530
Marta Górska <magorska.95@gmail.com>
2021-07-02 09:21:52 +0200

    Update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-   const apiRoot = 'https://damp-meadow-59806.herokuapp.com/v1/task';
-   const trelloApiRoot = 'https://damp-meadow-59806.herokuapp.com/v1/trello';
+   const apiRoot = 'https://damp-meadow-59806.herokuapp.com/v1/task/';
+   const trelloApiRoot = 'https://damp-meadow-59806.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
