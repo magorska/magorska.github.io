ad9d312b49d188ad0a71023b2349cb9cf09c0862
Marta Górska <magorska.95@gmail.com>
2021-07-20 10:42:14 +0200

    refactoring

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-   const apiRoot = 'https://damp-meadow-59806.herokuapp.com/v1/task/';
+   const apiRoot = 'https://damp-meadow-59806.herokuapp.com/v1/';
    const trelloApiRoot = 'https://damp-meadow-59806.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
